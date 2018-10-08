6eec060f86342ad9562c2866d70982c0f3185bab
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 01:30:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|i| i[0] == 'r'}
+end
+
+def contain_a(array)
+  na = []
+  array.each do |i|
+    if i.include?('a') 
+      na << i
+    end
+  end
+  na
+end 
+
+def first_wa(array)
+  array.each do |i|
+    if i.to_s.include?('wa')
+      return i 
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.keep_if {|i| i.class == String}
+end 
+
+def count_elements(array, element)
+  
+end 
